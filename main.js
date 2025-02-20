c5a3015b28d87bf43eb5bff68b20ff8bec4e9d06
dwaugh05 <dwaugh05@gmail.com>
2025-02-20 15:31:12 -0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+function createThread() {
+  fetch('https://webapp-ai-game.dwaugh05.workers.dev/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      messages: [{role: 'user', content: 'Hello, world!'}]
+    })
+  })
+  .then(res => res.json())
+  .then(data => console.log('Thread created:', data))
+  .catch(err => console.error('Error:', err));
+}
